11eb0bfab96db052d7d3876129941d4bdbd51f42
sarakh9 <sadat.kh9@gmail.com>
2025-01-11 21:39:00 +0330

    added check_user func

--- users_model.py
+++ users_model.py
@@ -8,15 +8,18 @@ class User:
     
     def create_table(self):
         self.cur.execute("""CREATE TABLE IF NOT EXISTS users(
-                          id PRIMARY KEY,
-                          username TEXT,
+                          username TEXT UNIQUE PRIMARY KEY,
                           password TEXT)""")
     
     def insert_into_users(self, user):
-        self.cur.execute("""INSERT OR IGNORE INTO users VALUES(?,?,?)""", user)
+        self.cur.execute("""INSERT OR IGNORE INTO users VALUES(?,?)""", user)
         self.conn.commit()
     
     def read_usernames(self):
         self.cur.execute("""SELECT username FROM users""")
         usernames = self.cur.fetchall()
         return usernames
+    def check_user(self, username):
+        self.cur.execute("""SELECT password FROM users WHERE username = ?""", (username,))
+        user = self.cur.fetchone()
+        return user
