8d1fe22ee3787b2e4a9d96489fbcb15e6f2bfd7f
sarakh9 <sadat.kh9@gmail.com>
2025-01-11 14:22:33 +0330

    standirdizing server side  messages, addin registration (needs a lot of changes)

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
-# import tools
+# import tools and modules
 import socket 
 import threading
+import hashlib
+from users_model import User
 
 # Defining constants IP and PORT
 IP = '127.0.0.1' # local host
 PORT = 15000
 CONNECTION_LIMIT = 10
+DISCONNECT_MSG = "Bye."
+REG_MSG = "REGISTERATION"
+LOGIN_MSG = "LOGIN"
 online_users = []
+db = User()
+
+lock = threading.Lock()
 
 # Function to send message to one client
 def send_msg_to_client(client, message):
@@ -31,29 +39,56 @@ def listen_for_msgs(client, username):
             send_msg([], final_msg)
 
         else:
-            print(f"Empty message from client {username}")
+            print(f"[EMPTY MSG] Empty message from client {username}")
 
 
 # Client handler function
 def client_handler(client):
     # Server will listen for client messages with max length of 2048
-    while True:
-        # Listening for username
-        username = client.recv(1024).decode('utf-8')
-        if username != '':
+    connected = True
+    while connected:
+        # Listening for command login or registration
+        command = client.recv(1024).decode('utf-8')
+        # check command is not emppty
+        if command != '':
+            # split command into its pieces
+            control, username, password = command.split(' ')
+            # if user wants to register
+            if control == REG_MSG:
+                # check if username is not being duplicated
+                duplicated_username_flag = 0
+                users = db.read_usernames()
+                for user in users:
+                    if username == user:
+                        duplicated_username_flag = 1
+                    if duplicated_username_flag:
+                        print(f"[DUPLICATION] Username '{username}' is duplicated")
+                        # !!!!send the message to the client
+                        break
+                # add new user to database
+                if duplicated_username_flag == 0:
+                    hashed_pswd = hashlib.md5(password.encode())
+                    with lock:
+                        db.insert_into_users((username, hashed_pswd))
+                        print("[REGISTRATION] User {username} added to database")
+                        # !!!!send user a message that they are registered and noe they  can login
+            elif command == LOGIN_MSG:
+                pass
+            else:
+                pass
             duplicated_username_flag = 0
             for user in online_users:
                 if username in user:
                     duplicated_username_flag = 1
             if duplicated_username_flag:
-                print(f"username '{username}' is duplicated")
+                print(f"[DUPLICATION] Username '{username}' is duplicated")
             else:
                 online_users.append((username, client))
                 new_user_msg = f"<CHATBOT> '{username}' joined the chat!"
                 send_msg([], new_user_msg)
                 break
         else:
-            print("username is empty")
+            print("[EMPTY MSG] Command is empty")
     # Because we don't want to have conflict with server listening for connection
     threading.Thread(target=listen_for_msgs, args=(client, username,)).start()
 
@@ -65,20 +100,27 @@ def main():
     # Bind server socket to port 15000 and ip 127.0.0.1
     try:
         serverSocket.bind((IP, PORT))
-        print("Server is running...")
+        print("[SERVER RUNNING]")
         print(f"SERVER IP:{IP} PORT: {PORT}")
     except Exception:
-        print(f"ERROR : Unable to bind to ip {IP} and port {PORT}")
+        print(f"[ERROR] Unable to bind to ip {IP} and port {PORT}")
     
     # Start listening on port 15000
     serverSocket.listen(CONNECTION_LIMIT)
-    print("The server is ready to receive")
+    print("[LISTENING...]")
 
     # Keep listening for client connection request on loop
     while True:
         # Accept connection request, create a connection socket and save the clients address(ip, port)
         connectionSocket, addr = serverSocket.accept()
-        print(f"Succesfully connected to client with ip: {addr[0]} and port: {addr[1]}")
+        print(f"[NEW CONNECTION] Succesfully connected to client with ip: {addr[0]} and port: {addr[1]}")
+        first_msg = F"""WELCOME TO SIMPLE TCP CHATROOM!
+HERE YOU CAN CREATE AN ACCOUNT, SEND PUBLICK AND PRIVATE MESSAGES.
+IF YOU'RE NEW HERE, TO START SEND THIS MESSAGE TO CREATE YOUR ACCOUNT: REGISTERATION <USERNAME> <PASSWORD>
+AND TO LOGIN SEND THIS MESSAGE: LOGIN <USERNAME> <PASSWORD>.
+NOTE: DO NOT USE SPACE IN YOUR USSERNAME AND IN YOUR PASSWORD!
+NOTE2: AFTER YOU REGISTERED YOU WILL NEED TO RESTART!"""
+        send_msg([('new_conn', connectionSocket)], first_msg)
 
         # Creat a thread for each connection to handle clients simultaneously 
         # and not to have conflict with server listening for connections
